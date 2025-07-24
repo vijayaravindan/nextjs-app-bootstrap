c57bc0365e0d846eb73bc6544d30c07ecb488e3e
vijayaravindan <85278047+vijayaravindan@users.noreply.github.com>
2025-07-24 22:10:34 +0530

    Add .next/static/chunks/src_app_settings_page_tsx_3e98992b._.js 1753375233321

---
+++ .next/static/chunks/src_app_settings_page_tsx_3e98992b._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_ef1b055b._.js"
+],
+    source: "dynamic"
+});
