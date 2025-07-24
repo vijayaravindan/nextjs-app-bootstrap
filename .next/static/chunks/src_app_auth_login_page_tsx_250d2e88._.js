01772401c264d861c526178b3c3a3a9dc49cbc75
vijayaravindan <85278047+vijayaravindan@users.noreply.github.com>
2025-07-24 22:10:11 +0530

    Add .next/static/chunks/src_app_auth_login_page_tsx_250d2e88._.js 1753375210924

---
+++ .next/static/chunks/src_app_auth_login_page_tsx_250d2e88._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_b5194b51._.js"
+],
+    source: "dynamic"
+});
