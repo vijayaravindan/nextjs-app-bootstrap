abc206bc60b19411fa53c5b1ac430b81f56fb4c5
vijayaravindan <85278047+vijayaravindan@users.noreply.github.com>
2025-07-24 22:10:01 +0530

    Add .next/static/chunks/src_app_dashboard_page_tsx_250d2e88._.js 1753375200833

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_250d2e88._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_lodash_90f72504._.js",
+  "static/chunks/node_modules_recharts_es6_3fa6f35d._.js",
+  "static/chunks/node_modules_aad59dbc._.js",
+  "static/chunks/src_2d0e79dc._.js"
+],
+    source: "dynamic"
+});
